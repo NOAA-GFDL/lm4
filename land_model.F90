fdd75ac4706477e4b942f00c3710e1bdd79f262d
Sergey Malyshev <Sergey.Malyshev@noaa.gov>
2015-10-19 12:09:50 -0400

    Added initialization of astronomy, in case atmos doesn't do it

--- land_model.F90
+++ land_model.F90
@@ -29,7 +29,7 @@ use diag_manager_mod, only : diag_axis_init, register_static_field, &
      register_diag_field, send_data, diag_field_add_attribute
 use constants_mod, only : radius, hlf, hlv, hls, tfreeze, pi, rdgas, rvgas, cp_air, &
      stefan
-use astronomy_mod, only : diurnal_solar
+use astronomy_mod, only : astronomy_init, diurnal_solar
 use sphum_mod, only : qscomp
 use tracer_manager_mod, only : NO_TRACER, get_tracer_index, get_tracer_names
 
@@ -308,6 +308,8 @@ subroutine land_model_init &
   landInitClock  = mpp_clock_id('Land init'          ,CLOCK_FLAG_DEFAULT,CLOCK_ROUTINE)
 
   call mpp_clock_begin(landInitClock)
+
+  call astronomy_init ! in case it is not initialized, e.g. when using atmos_null
 
   ! [ ] initialize land debug output
   call land_debug_init()
