afc2b311cef5c4e3d164e44fb84d73d7f6e15c90
Sergey Malyshev <Sergey.Malyshev@noaa.gov>
2017-12-11 18:10:55 -0500

    OpenMP-related fix: add n_cohorts to the list of private vars.

--- land_model.F90
+++ land_model.F90
@@ -1066,7 +1066,7 @@ subroutine update_land_model_fast ( cplr2land, land2cplr )
 !$OMP parallel do default(none) shared(lnd,land_tile_map,cplr2land,land2cplr,phot_co2_overridden, &
 !$OMP                                  phot_co2_data,runoff,runoff_c,id_area,id_z0m,id_z0s,       &
 !$OMP                                  id_Trad,id_Tca,id_qca,isphum,id_cd_m,id_cd_t) &
-!$OMP                                  private(i1,i,j,k,ce,tile,ISa_dn_dir,ISa_dn_dif)
+!$OMP                                  private(i1,i,j,k,ce,tile,ISa_dn_dir,ISa_dn_dif,n_cohorts)
   do l = lnd%ls, lnd%le
      i = lnd%i_index(l)
      j = lnd%j_index(l)
