cd6799f686b3a75bb7a6386c5abe0643f599557d
Thomas Robinson <Thomas.Robinson@noaa.gov>
2020-04-27 12:36:13 -0400

    Adds LGPL to all source files

--- glacier/glac_tile.F90
+++ glacier/glac_tile.F90
@@ -1,3 +1,21 @@
+!***********************************************************************
+!*                   GNU Lesser General Public License
+!*
+!* This file is part of the GFDL Land Model 4 (LM4).
+!*
+!* LM4 is free software: you can redistribute it and/or modify it under
+!* the terms of the GNU Lesser General Public License as published by
+!* the Free Software Foundation, either version 3 of the License, or (at
+!* your option) any later version.
+!*
+!* LM4 is distributed in the hope that it will be useful, but WITHOUT
+!* ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
+!* FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General Public License
+!* for more details.
+!*
+!* You should have received a copy of the GNU Lesser General Public
+!* License along with LM4.  If not, see <http://www.gnu.org/licenses/>.
+!***********************************************************************
 module glac_tile_mod
 #include <fms_platform.h>
 
