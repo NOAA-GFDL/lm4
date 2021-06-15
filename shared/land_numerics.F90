df55caf04432a73074ebb7bb47667101f8fd7b71
Sergey Malyshev <Sergey.Malyshev@noaa.gov>
2021-06-15 00:14:57 -0400

    Add a function calculating imaginary error function erfi

--- shared/land_numerics.F90
+++ shared/land_numerics.F90
@@ -43,6 +43,8 @@ public :: gammaL, gammaU ! incomplete gamma
 public :: gamma ! because ifort 11 does not have built-in gamma
 public :: gammaln ! ln(gamma); useful for ratio of gamma functions
 
+public :: erfi ! imaginary error function
+
 public :: rank_descending ! rank the input array in descending order
 
 public :: numerics_init
@@ -89,6 +91,10 @@ type :: horiz_remap_type
        dstPE(:) => NULL()    ! PE that requests and then uses the data
 end type horiz_remap_type
 
+! constants and precomputed coefficients of Dawson function
+integer, parameter :: DAWSON_NMAX = 6
+real,    parameter :: DAWSON_H    = 0.4
+real               :: dawson_c(DAWSON_NMAX) ! coefficients precomputed on initialization
 
 contains ! -=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-
 
@@ -96,8 +102,15 @@ contains ! -=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-
 ! ============================================================================
 ! Initializes the numerics module.
 subroutine numerics_init()
+  integer :: i
+
   module_is_initialized =.TRUE.
   call log_version(version, mod_name, thisfile)
+
+  ! initialize Dawson function pre-computed coefficients
+  do i = 0, DAWSON_NMAX-1
+     dawson_c(i+1)=exp(-(DAWSON_H*(2.0*i+1.0))**2)
+  enddo
 end subroutine numerics_init
 
 
@@ -1577,6 +1590,53 @@ real function gammaln(xx)
 end function gammaln
 
 ! ==============================================================================
+! returns imaginary error function: \int_0^x \exp(x^2) dx
+real function erfi(x)
+   real :: x
+   erfi = exp(x**2)*dawson(x)
+end function erfi
+
+! ==============================================================================
+! returns Dawson's integral: \exp(-x^2) \int_0^x \exp(x^2) dx
+! W. H. Press, S.A. Teukolsky, W.T. Vetterling, and B.P. Flannery.
+! Numerical Recipes. The Art of Scientific Computing. 3rd Ed.
+! Cambridge University Press, 2007.
+real function dawson(x) result(ans)
+  real, intent(in) :: x
+
+  real, parameter :: A1=2.0/3.0, A2=0.4, A3=2.0/7.0
+  real :: d1,d2,e1,e2,sum,x2,xp,xx
+  integer :: i, n0
+
+  if(.not.module_is_initialized) then
+    call my_error(mod_name,'module was not initialized',FATAL)
+  endif
+
+  if (abs(x) < 0.2) then
+     ! Use series expansion.
+     x2=x*x
+     ans=x*(1.0-A1*x2*(1.0-A2*x2*(1.0-A3*x2)));
+  else
+     ! Use sampling theorem representation.
+     xx=abs(x)
+     n0=2*int(0.5*xx/DAWSON_H+0.5)
+     xp=xx-n0*DAWSON_H
+     e1=exp(2.0*xp*DAWSON_H)
+     e2=e1*e1
+     d1=n0+1
+     d2=d1-2.0
+     sum=0.0
+     do i = 1, size(dawson_c)
+        sum = sum + dawson_c(i)*(e1/d1+1.0/(d2*e1))
+        d1 = d1 + 2.0
+        d2 = d2 - 2.0
+        e1 = e1*e2
+     enddo
+     ans=0.5641895835*SIGN(exp(-xp*xp),x)*sum;
+  endif
+end function dawson
+
+! ==============================================================================
 ! Reports error, including file name and line.
 subroutine my_error(mod_name, message, mode, file, line)
 
@@ -1596,6 +1656,5 @@ subroutine my_error(mod_name, message, mode, file, line)
   endif
   call error_mesg(mod_name, mesg, mode)
 end subroutine
-
 
 end module land_numerics_mod
