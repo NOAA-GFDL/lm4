939041d09f2d958ad19b5343927434938c7bdcbb
Sergey Malyshev <Sergey.Malyshev@noaa.gov>
2021-10-05 16:15:51 -0400

    Do soil N deposition for CORPSE only
    Since it currently depends on CORPSE data structures.

--- vegetation/vegetation.F90
+++ vegetation/vegetation.F90
@@ -84,8 +84,8 @@ use vegn_harvesting_mod, only : &
      vegn_harvesting_init, vegn_harvesting_end, vegn_harvesting, crop_seed_transport
 use vegn_fire_mod, only : vegn_fire_init, vegn_fire_end, update_fire_data, fire_option, FIRE_LM3
 use soil_carbon_mod, only : soil_carbon_option, SOILC_CORPSE, SOILC_CORPSE_N, &
-     N_C_TYPES, C_FAST, C_SLOW, c_shortname, c_longname, &
-     add_litter, soil_NH4_deposition, soil_NO3_deposition, soil_org_N_deposition, &
+     SOILC_CENTURY, SOILC_CENTURY_BY_LAYER, N_C_TYPES, C_FAST, C_SLOW, c_shortname, c_longname, &
+     soil_NH4_deposition, soil_NO3_deposition, soil_org_N_deposition, &
      cull_cohorts
 use vegn_util_mod, only: kill_small_cohorts_ppa
 
@@ -2155,15 +2155,22 @@ subroutine vegn_step_3(vegn, soil, cana_T, precip, ndep_nit, ndep_amm, ndep_org,
      __DEBUG3__(depth_ave, tsoil, theta)
   endif
 
-  ! Do N deposition first. For now, it all goes to leaf litter
-  ! slm, ens 20180523: in contrast to original bns design, N deposition (which includes
-  ! both deposition from the atmosphere and fertilization) now goes into upper soil layer.
   call check_var_range(ndep_amm, 0.0, HUGE(1.0), 'vegn_step_3', 'ndep_amm', FATAL)
   call check_var_range(ndep_nit, 0.0, HUGE(1.0), 'vegn_step_3', 'ndep_nit', FATAL)
   call check_var_range(ndep_org, 0.0, HUGE(1.0), 'vegn_step_3', 'ndep_org', FATAL)
-  call soil_NH4_deposition   (ndep_amm*dt_fast_yr, soil%org_matter(1))
-  call soil_NO3_deposition   (ndep_nit*dt_fast_yr, soil%org_matter(1))
-  call soil_org_N_deposition (ndep_org*dt_fast_yr, soil%org_matter(1))
+  select case(soil_carbon_option)
+  case (SOILC_CENTURY, SOILC_CENTURY_BY_LAYER)
+     ! do nothing for now
+  case (SOILC_CORPSE, SOILC_CORPSE_N)
+     ! Do N deposition first. For now, it all goes to leaf litter
+     ! slm, ens 20180523: in contrast to original bns design, N deposition (which includes
+     ! both deposition from the atmosphere and fertilization) now goes into upper soil layer.
+     call soil_NH4_deposition   (ndep_amm*dt_fast_yr, soil%org_matter(1))
+     call soil_NO3_deposition   (ndep_nit*dt_fast_yr, soil%org_matter(1))
+     call soil_org_N_deposition (ndep_org*dt_fast_yr, soil%org_matter(1))
+  case default
+     call error_mesg('soil_step_2', 'unrecognized soil carbon option -- this should never happen', FATAL)
+  end select
 
   soil%gross_nitrogen_flux_into_tile = soil%gross_nitrogen_flux_into_tile + (ndep_amm+ndep_nit+ndep_org)*dt_fast_yr
 
