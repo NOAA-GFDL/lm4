4c121c300cbaf13a4bddf6b2d36d8d335a58246d
Sergey Malyshev <Sergey.Malyshev@noaa.gov>
2022-03-20 18:06:37 -0400

    Change names of CENTURY litter fields to avoid clash with differently-dimensioned CORPSE fields

--- soil/soil.F90
+++ soil/soil.F90
@@ -652,10 +652,12 @@ subroutine soil_init ( id_ug, id_band, id_zfull )
            endif
            call free_land_restart(restart1)
         endif
-        if (field_exists(restart,'leaf_litter_fast_C')) then
+        ! name is deliberately different from similar CORPSE fields so that we can start
+        ! with CORPSE restarts with zero litter
+        if (field_exists(restart,trim(l_shortname(1))//'_litt_'//trim(c_shortname(1))//'_C')) then
            do i = 1, N_C_TYPES
               do k = 1, N_LITTER_POOLS
-                 call get_tile_data(restart,trim(l_shortname(k))//'_litter_'//trim(c_shortname(i))//'_C',litter_century_C_ptr,i,k)
+                 call get_tile_data(restart,trim(l_shortname(k))//'_litt_'//trim(c_shortname(i))//'_C',litter_century_C_ptr,i,k)
               enddo
            enddo
         endif
@@ -1517,7 +1519,7 @@ subroutine save_soil_restart (tile_dim_length, timestamp)
      call add_tile_data(restart,'ssc', 'zfull', soil_slow_soil_C_ptr ,'slow soil carbon', 'kg C/m2')
      do i = 1, N_C_TYPES
         do k = 1,N_LITTER_POOLS
-           call add_tile_data(restart,trim(l_shortname(k))//'_litter_'//trim(c_shortname(i))//'_C',litter_century_C_ptr,i,k,trim(l_longname(k))//' litter '//trim(c_longname(i))//' C','kg/m2')
+           call add_tile_data(restart,trim(l_shortname(k))//'_litt_'//trim(c_shortname(i))//'_C',litter_century_C_ptr,i,k,trim(l_longname(k))//' litter '//trim(c_longname(i))//' C','kg/m2')
         enddo
      enddo
 
