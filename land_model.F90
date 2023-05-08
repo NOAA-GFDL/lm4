e8382ad481c55c1f85a7282b06f3f8e49d7b96b3
Enrico Zorzetto <enrico.zorzetto@noaa.gov>
2023-05-08 13:06:11 -0400

    fixed typo V

--- land_model.F90
+++ land_model.F90
@@ -1482,7 +1482,7 @@ subroutine update_land_model_fast_0d ( tile, l,itile, N, land2cplr, &
    phot_co2_overridden, phot_co2_data, &
    runoff, runoff_c, &
    drydep, wetdep  & ! EZSNOW added
-   )
+  )
   type (land_tile_type), pointer :: tile
   integer, intent(in) :: l ! position in unstructured grid
   integer, intent(in) :: itile ! tile number
@@ -2878,7 +2878,8 @@ endif
    ! ------ what to do for quantities averaged monthly and regridded over snow partial cover? 
    ! ------ Here they are saved weighted by the fractional snow cover
    ! recompute the snow area frac and near surface properties here to get that at end of snow processes calculations
-  call snow_get_depth_area ( tile%snow, tile%snow%sp%depth(), snow_area )
+!   call snow_get_depth_area ( tile%snow, tile%snow%sp%depth(), snow_area )
+  snow_area = tile%snow%sp%area()
   call tile%snow%sp%nearsurf_properties()
    ! if(tile%snow%nlayers > 0) then
   call send_tile_data(id_snow_avrg_optd, snow_area * tile%snow%sp%avrg_optd(), tile%diag)
