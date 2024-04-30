5f6ad7b95ffff52a5a4f77b5741fb5c7d2d4ec14
Sergey Malyshev <Sergey.Malyshev@noaa.gov>
2024-04-30 10:30:35 -0400

    Move GLASS snow accessors into gl_snow.F90

--- snow/gl_snow/gl_snow.F90
+++ snow/gl_snow/gl_snow.F90
@@ -22,13 +22,12 @@ use land_tile_io_mod, only: land_restart_type, &
      add_restart_axis, add_tile_data, get_tile_data, get_tile_by_idx, &
      add_int_tile_data, get_int_tile_data, field_exists
 use land_debug_mod, only : is_watch_point, check_var_range
-use snowpack_mod, only: snowpack_init_lm4p2, read_snowpack_namelist
-use snow_accessors_mod ! use everything
+use snowpack_mod, only: snowpack_init_lm4p2, read_snowpack_namelist, &
+     snow_layer_type, MAX_OPT_LAYERS, cpw, clw, csw
 use snowlayers_io_mod, only :  read_create_snowlayers, create_snowlayer_dimension, &
      add_snowlayer_data, add_int_snowlayer_data, get_snowlayer_data, get_int_snowlayer_data
 use snow_evolution_mod, only : gl_snow_step_2, gl_sweep_tiny_snow, &
          read_F06_data, gl_compute_snow_albedo, read_snow_evolution_namelist, use_internal_sources
-use snowpack_mod, only : snow_layer_type, MAX_OPT_LAYERS, cpw, clw, csw
 use fms_io_mod,       only : restart_file_type, get_instance_filename
 
 
@@ -519,6 +518,61 @@ subroutine cm_snow_ws_ptr(tile, i, ptr)
    endif
 end subroutine cm_snow_ws_ptr
 
+! ============================================================================
+! snowlayer accessor functions: given a pointer to a snowlayer, return a pointer to a
+! specific member of the snowlayer structure
+
+#define DEFINE_SNOWPACK_ACCESSOR_0D(xtype,x) subroutine snowtile_ ## x ## _ptr(t,p);\
+type(land_tile_type),pointer::t;xtype,pointer::p;p=>NULL();if(associated(t))then;if(associated(t%snow))p=>t%snow%sp%x;endif;end subroutine
+
+#define DEFINE_SNOWLAYER_ACCESSOR(xtype,x) subroutine snowlayer_ ## x ## _ptr(c,p);\
+type(snow_layer_type),pointer::c;xtype,pointer::p;p=>NULL();if(associated(c))p=>c%x;end subroutine
+
+#define DEFINE_SNOWLAYER_ACCESSOR_BC(xtype,x) subroutine snowlayer_ ## x ## _bc_ptr(c,p);\
+type(snow_layer_type),pointer::c;xtype,pointer::p;p=>NULL();if(associated(c))p=>c%x(1);end subroutine
+
+#define DEFINE_SNOWLAYER_ACCESSOR_MD(xtype,x) subroutine snowlayer_ ## x ## _md_ptr(c,p);\
+type(snow_layer_type),pointer::c;xtype,pointer::p;p=>NULL();if(associated(c))p=>c%x(2);end subroutine
+
+#define DEFINE_SNOWLAYER_ACCESSOR_OM(xtype,x) subroutine snowlayer_ ## x ## _om_ptr(c,p);\
+type(snow_layer_type),pointer::c;xtype,pointer::p;p=>NULL();if(associated(c))p=>c%x(3);end subroutine
+
+
+
+DEFINE_SNOWPACK_ACCESSOR_0D(integer, nlayers)
+DEFINE_SNOWPACK_ACCESSOR_0D(real, topwater)
+DEFINE_SNOWPACK_ACCESSOR_0D(real, topwheat)
+DEFINE_SNOWPACK_ACCESSOR_0D(real, topsnowdeficit)
+DEFINE_SNOWPACK_ACCESSOR_0D(real, topsnowheatdeficit)
+
+DEFINE_SNOWPACK_ACCESSOR_0D(real, nearsurf_bceq_tot)
+DEFINE_SNOWPACK_ACCESSOR_0D(real, nearsurf_bceq_em)
+DEFINE_SNOWPACK_ACCESSOR_0D(real, nearsurf_bceq_im)
+DEFINE_SNOWPACK_ACCESSOR_0D(real, nearsurf_dendr)
+DEFINE_SNOWPACK_ACCESSOR_0D(real, nearsurf_optd)
+DEFINE_SNOWPACK_ACCESSOR_0D(real, nearsurf_sph)
+DEFINE_SNOWPACK_ACCESSOR_0D(real, nearsurf_rho)
+DEFINE_SNOWPACK_ACCESSOR_0D(real, nearsurf_age)
+DEFINE_SNOWPACK_ACCESSOR_0D(real, nearsurf_T)
+
+
+DEFINE_SNOWLAYER_ACCESSOR(real,T)
+DEFINE_SNOWLAYER_ACCESSOR(real,wl)
+DEFINE_SNOWLAYER_ACCESSOR(real,ws)
+DEFINE_SNOWLAYER_ACCESSOR(real,dz)
+DEFINE_SNOWLAYER_ACCESSOR(real,age)
+DEFINE_SNOWLAYER_ACCESSOR(real,dendr)
+DEFINE_SNOWLAYER_ACCESSOR(real,optd)
+DEFINE_SNOWLAYER_ACCESSOR(real,sph)
+
+
+
+DEFINE_SNOWLAYER_ACCESSOR_BC(real,wc_im)
+DEFINE_SNOWLAYER_ACCESSOR_MD(real,wc_im)
+DEFINE_SNOWLAYER_ACCESSOR_OM(real,wc_im)
+DEFINE_SNOWLAYER_ACCESSOR_BC(real,wc_em)
+DEFINE_SNOWLAYER_ACCESSOR_MD(real,wc_em)
+DEFINE_SNOWLAYER_ACCESSOR_OM(real,wc_em)
 
 end module gl_snow_mod
 
