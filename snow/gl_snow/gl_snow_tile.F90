04f8e1b480e5c9fdc79d42206324a218fa3b84b4
Sergey Malyshev <Sergey.Malyshev@noaa.gov>
2024-06-21 12:17:25 -0400

    Replace textual comparisons with integer options
    Textual namelist options are parsed on initialization, and resulting
    integer constants are used at the run-rime to differentiate between
    cases.
    
    This is to improve performance a bit: textual conversions (trim,
    lowercase) and string comparisons are far from cost-free in FORTRAN, and
    calling them every time step fror every land model tile can accumulate
    and lead to unwanted performance degradation. Also "select case"
    statements are presumably faster (probably negligibly) and better
    reflect the structure of the code.
    
    Besides, parsing options at the initialization and using integer
    constants allows for better flexibility in case it is deemed better to
    change option names, or introduce aliases -- the change only needs to be
    don in the parsing code.

--- snow/gl_snow/gl_snow_tile.F90
+++ snow/gl_snow/gl_snow_tile.F90
@@ -4,7 +4,7 @@ module gl_snow_tile_mod
 
 use mpp_mod, only: input_nml_file
 
-use fms_mod, only : FATAL, lowercase
+use fms_mod, only : FATAL
 use constants_mod, only : tfreeze, hlf
 
 use land_constants_mod, only : NBANDS
@@ -20,7 +20,7 @@ use snowpack_mod, only : snow_layer_type, snowpack_t
 use snow_tile_mod, only: snow_tile_type, NTRACERS, z0_momentum, &
      k_over_B, cpw, clw, csw, snow_data_area, snow_radiation
 use snow_evolution_mod, only : gl_sweep_tiny_snow, assign_substrate_sw_to_surface, &
-     albedo_to_use, use_internal_sources, thresh_snow_depth_swheat, gl_compute_snow_albedo, &
+     albedo_option, ALBEDO_SNICAR, use_internal_sources, thresh_snow_depth_swheat, gl_compute_snow_albedo, &
      gl_snow_step_2_ev => gl_snow_step_2, delta_time, do_mgimplicit, gl_sweep_huge_snow
 
 
@@ -937,7 +937,7 @@ subroutine gl_partition_sw( &
    if (ALLOCATED(snow%sp%swheat)) DEALLOCATE(snow%sp%swheat)
 
    ! slm: does the code below assume that there are always will be 2 bands?
-   if (trim(lowercase(albedo_to_use))=='snicar') then
+   if (albedo_option==ALBEDO_SNICAR) then
       if ((use_internal_sources) .and. ((snow%sp%depth() > thresh_snow_depth_swheat) &
                                  .and. (snow%sp%nlayers > 0))) then
          ALLOCATE(snow%sp%swheat(snow%sp%nlayers))
